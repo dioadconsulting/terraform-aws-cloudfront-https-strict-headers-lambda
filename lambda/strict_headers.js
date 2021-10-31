975a66f5c086294d21bb6e5d5d14c78a057bc58a
Patrick Downey <patrick.downey@dioadconsulting.com>
2021-10-31 22:14:34 +0000

    fix up null content-security-policy

--- lambda/strict_headers.js
+++ lambda/strict_headers.js
@@ -4,34 +4,38 @@
 
 
 exports.handler = (event, context, callback) => {
-    
+ 
   //Get contents of response
   const response = event.Records[0].cf.response;
   const headers = response.headers;
 
-  //Set new headers 
+  //Set new headers
   // https://infosec.mozilla.org/guidelines/web_security#http-strict-transport-security
   headers['strict-transport-security'] = [
     {
       key: 'Strict-Transport-Security',
       value: 'max-age=63072000; includeSubdomains; preload'
     }
-  ]; 
+  ];
 
   // https://infosec.mozilla.org/guidelines/web_security#content-security-policy
-  headers['content-security-policy'] = [
+  if ( headers['content-security-policy'] === undefined ) {
+    headers['content-security-policy'] = []
+  }
+
+  headers['content-security-policy'].push(
     {
       key: 'Content-Security-Policy',
       value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
     }
-  ]; 
+  );
 
   // https://infosec.mozilla.org/guidelines/web_security#x-content-type-options
   headers['x-content-type-options'] = [
     {
       key: 'X-Content-Type-Options', value: 'nosniff'
     }
-  ]; 
+  ];
 
   // https://infosec.mozilla.org/guidelines/web_security#x-frame-options
   headers['x-frame-options'] = [
@@ -39,7 +43,7 @@ exports.handler = (event, context, callback) => {
       key: 'X-Frame-Options',
       value: 'DENY'
     }
-  ]; 
+  ];
 
   // https://infosec.mozilla.org/guidelines/web_security#x-xss-protection
   headers['x-xss-protection'] = [
@@ -47,16 +51,16 @@ exports.handler = (event, context, callback) => {
       key: 'X-XSS-Protection',
       value: '1; mode=block'
     }
-  ]; 
+  ];
 
-  // https://infosec.mozilla.org/guidelines/web_security#referrer-policy 
+  // https://infosec.mozilla.org/guidelines/web_security#referrer-policy
   headers['referrer-policy'] = [
     {
       key: 'Referrer-Policy',
       value: 'same-origin'
     }
-  ]; 
-    
+  ];
+
   //Return modified response
   callback(null, response);
 };
